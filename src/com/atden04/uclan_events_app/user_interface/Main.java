c31076c8ad27700efff86ada3f4e293b4772094b
Alexander Denton <atdenton2004@gmail.com>
2024-03-01 13:31:47 +0000

    Added comments

--- src/com/atden04/uclan_events_app/user_interface/Main.java
+++ src/com/atden04/uclan_events_app/user_interface/Main.java
@@ -11,39 +11,72 @@ import javafx.stage.Stage;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+/**
+ * Main class that extends the JavaFX Application class
+ */
 public class Main extends Application {
 
+    /**
+     * Object for the model of the program
+     */
     Model model;
+    /**
+     * Object for the controller of the program
+     */
     Controller controller;
+    /**
+     * Object for the parser to read in data from csv files to populate the model
+     */
     modelParser parser;
 
+    /**
+     * Start function for the JavaFX which creates and initialises the objects within the main class
+     * Loads in the fxml file for the mainScene and imports the stylesheet.
+     * @param stage Window where the user interacts with the application
+     * @throws Exception Throws Exception if program can't load fxml file
+     */
     @Override
     public void start(Stage stage) throws Exception {
 
+        //Create attributes
         this.parser = new modelParser();
         this.model = new Model();
         this.controller = new Controller();
 
+        //Load in the fxml file
         FXMLLoader loader = new FXMLLoader(ResourceManager.getFxml("mainScene.fxml"));
         loader.setControllerFactory((Klass) -> this.controller);
 
+        //set up the scene to create elements to
         Parent root = loader.load();
         Scene scene = new Scene(root);
         scene.getStylesheets().add(ResourceManager.getCss("style.css").toExternalForm());
 
+        //Initialise controller and model.
+        //This happens after constructor, so you can supply both objects with references to the other.
+        //You can't do this with null objects.
         this.controller.initialise(this.model, stage);
         this.model.initialise(this.controller,this.parser);
 
+        //Set title of the Window
         stage.setTitle("UCLan Events");
         stage.setScene(scene);
         stage.show();
     }
 
+    /**
+     * Override to handle code when closing the app but before the program finishes.
+     */
     @Override
     public void stop() {
         System.out.println("Closing the App..");
     }
 
+    /**
+     * Main function that calls the launch of javaFX libraries
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }
--- src/com/atden04/uclan_events_app/user_interface/Model.java
+++ src/com/atden04/uclan_events_app/user_interface/Model.java
@@ -8,14 +8,38 @@ import javafx.collections.ObservableList;
 
 import java.util.Objects;
 
+/**
+ * Class for the Model, helping to implement MVC Design Pattern
+ */
 public class Model {
+    /**
+     * Reference to controller to interact with user interface
+     */
     public Controller controller;
+    /**
+     * Reference to parser for the Model
+     */
     private modelParser parser;
+    /**
+     * ObservableList containing all the Event objects
+     */
     private ObservableList<Event> events;
+    /**
+     * ObservableList containing all the User objects
+     */
     private ObservableList<User> users;
+    /**
+     * Attribute containing reference to current user object
+     */
     private User currentUser;
+    /**
+     * Boolean attribute to indicate if user is logged in
+     */
     private boolean isUserLoggedIn;
 
+    /**
+     * Constructor for the Model
+     */
     public Model()
     {
         System.out.println("Model Constructed");
@@ -23,6 +47,11 @@ public class Model {
         this.users = FXCollections.observableArrayList();
     }
 
+    /**
+     * Public method to initialise the Model
+     * @param controller reference to the controller object
+     * @param parser reference to the parser obejct
+     */
     public void initialise(Controller controller, modelParser parser) {
         this.isUserLoggedIn = false;
         this.controller = controller;
@@ -32,19 +61,39 @@ public class Model {
         this.parser.parseUsers("C:\\Users\\Alexa\\IdeaProjects\\UCLanHackathonEventsApp\\src\\com\\atden04\\uclan_events_app\\res\\users.csv", this.users);
     }
 
+    /**
+     * Method to register an event to the current User.
+     * @param event reference to the inputted event object
+     */
     public void registerEvent(Event event)
     {
         currentUser.registerEvent(event);
     }
 
+    /**
+     * Method to remove an event from the current User.
+     * This means they will no longer receive notifications for this event.
+     * @param event reference to the inputted event object
+     */
     public void unregisterEvent(Event event) {
         currentUser.unregisterEvent(event);
     }
 
+    /**
+     * Getter to check if user is logged in.
+     * @return attribute to indicate if user is logged in.
+     */
     public boolean isUserLoggedIn() {
         return isUserLoggedIn;
     }
 
+    /**
+     * Method to log in user. Check's the details inputted and will indicate if user is registered,
+     * or if the password is correct if they are registered or if they've successfully logged in.
+     * @param inputtedEmail string containing the inputted email
+     * @param inputtedPassword string containing the inputted password
+     * @return string message to output to user indicating outcome of the log in
+     */
     public String loginUser(String inputtedEmail, String inputtedPassword) {
         String returnStr = new String("");
         if (this.isEmailRegisterd(inputtedEmail)) {
@@ -66,6 +115,11 @@ public class Model {
         return returnStr;
     }
 
+    /**
+     * Private method to check if user's email is registered
+     * @param email string containing user's email
+     * @return boolean value to indicate if user is registered
+     */
     private boolean isEmailRegisterd(String email) {
         boolean returnBool = false;
         for (User u : users) {
@@ -76,6 +130,10 @@ public class Model {
         return returnBool;
     }
 
+    /**
+     * Public method to return the current user.
+     * @return reference to current user object.
+     */
     public User getCurrentUser() {
         return this.currentUser;
     }
